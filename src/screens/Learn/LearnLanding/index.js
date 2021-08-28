13eec551cbfda6cd6151130916d8785166ef3e0b
Ren Xiang <foorenxiang@gmail.com>
2021-08-29 06:21:42 +0800

    added recycle button

--- src/screens/Learn/LearnLanding/index.js
+++ src/screens/Learn/LearnLanding/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import { Image, ScrollView, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Image, View, ScrollView, TouchableOpacity } from 'react-native';
 import LearnImage from './LearnLanding.png';
+import RecycleUpImage from './recycle_up.png';
+import RecycleDownImage from './recycle_down.png';
 
 const LearnLanding = ({ navigation: { navigate } }) => {
+  const [recycleBtnSelected, setRecycleBtnSelected] = useState(false);
+  const recycleHandler = () => {
+    setRecycleBtnSelected(!recycleBtnSelected);
+  };
   return (
     <ScrollView
       style={{ flex: 1, width: '100%' }}
-      contentContainerStyle={{ flex: 1, width: '100%' }}
+      contentContainerStyle={{ flex: 1, width: '100%', backgroundColor: '#000' }}
     >
       <Image
         source={LearnImage}
-        style={{ flex: 1, width: '100%', height: 400, resizeMode: 'center' }}
+        style={{
+          flex: 1,
+          width: '100%',
+          height: 400,
+          resizeMode: 'center',
+          opacity: recycleBtnSelected ? 0.2 : 1,
+        }}
       />
       <TouchableOpacity
         style={{
@@ -25,6 +37,19 @@ const LearnLanding = ({ navigation: { navigate } }) => {
         }}
         onPress={() => navigate('LearnArticleViewer')}
       />
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          top: 570,
+          left: 150,
+          elevation: 200,
+          height: 110,
+          width: 325,
+        }}
+        onPress={() => recycleHandler()}
+      >
+        <Image source={RecycleUpImage} />
+      </TouchableOpacity>
     </ScrollView>
   );
 };
