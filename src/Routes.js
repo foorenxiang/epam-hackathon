a7dcfe7ac380b902b365be4c3965f97089a648d7
Ren Xiang <foorenxiang@gmail.com>
2021-08-28 13:53:37 +0800

    fixed tabnavigator nesting

--- src/Routes.js
+++ src/Routes.js
@@ -1,43 +1,42 @@
 import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './screens/Boilerplate';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreenTabs from './screens/Home';
 import Learn from './screens/Learn';
 import Log from './screens/Log';
 import Profile from './screens/Profile';
 import AppContext from './utils/AppContext';
 
-const { Navigator: TabNavigator, Screen: TabScreen } = createBottomTabNavigator();
+const { Navigator: StackNavigator, Screen: StackScreen } = createNativeStackNavigator();
 const { Navigator: DrawerNavigator, Screen: DrawerScreen } = createDrawerNavigator();
 
-const MainScreen = () => (
-  <TabNavigator initialRouteName="Home">
-    <TabScreen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
-    <TabScreen name="Log" component={Log} options={{}} />
-    <TabScreen name="Learn" component={Learn} options={{}} />
-    <TabScreen name="Profile" component={Profile} options={{}} />
-  </TabNavigator>
-);
-
-const Routes = () => {
+const MainScreen = () => {
   const { headerTitle } = useContext(AppContext);
-
   return (
-    <NavigationContainer>
-      <DrawerNavigator
-        initialRouteName="Main"
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <DrawerScreen name="Main" component={MainScreen} options={{ title: headerTitle }} />
-        <DrawerScreen name="Log" component={Log} options={{}} />
-        <DrawerScreen name="Learn" component={Learn} options={{}} />
-        <DrawerScreen name="Profile" component={Profile} options={{}} />
-      </DrawerNavigator>
-    </NavigationContainer>
+    <StackNavigator initialRouteName="HomeScreenTabs" screenOptions={{ title: headerTitle }}>
+      <StackScreen name="HomeScreenTabs" component={HomeScreenTabs} />
+      <StackScreen name="Log" component={Log} />
+      <StackScreen name="Learn" component={Learn} />
+      <StackScreen name="Profile" component={Profile} />
+    </StackNavigator>
   );
 };
+
+const Routes = () => (
+  <NavigationContainer>
+    <DrawerNavigator
+      initialRouteName="Main"
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <DrawerScreen name="Main" component={MainScreen} />
+      <DrawerScreen name="Log" component={Log} options={{}} />
+      <DrawerScreen name="Learn" component={Learn} options={{}} />
+      <DrawerScreen name="Profile" component={Profile} options={{}} />
+    </DrawerNavigator>
+  </NavigationContainer>
+);
 
 export default Routes;
