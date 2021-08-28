6ca0096733d0229f587ac71370c6027e205a2180
Ren Xiang <foorenxiang@gmail.com>
2021-08-29 06:34:39 +0800

    added recycle wheel

--- src/screens/Learn/LearnLanding/index.js
+++ src/screens/Learn/LearnLanding/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Image, View, ScrollView, TouchableOpacity } from 'react-native';
 import LearnImage from './LearnLanding.png';
 import RecycleUpImage from './recycle_up.png';
-import RecycleDownImage from './recycle_down.png';
+import RecycleWheel from './recycle_wheel.png';
 
 const LearnLanding = ({ navigation: { navigate } }) => {
   const [recycleBtnSelected, setRecycleBtnSelected] = useState(false);
@@ -37,14 +37,34 @@ const LearnLanding = ({ navigation: { navigate } }) => {
         }}
         onPress={() => navigate('LearnArticleViewer')}
       />
+      {recycleBtnSelected && (
+        <TouchableOpacity
+          style={{
+            position: 'absolute',
+            top: 430,
+            left: -17,
+            elevation: 100,
+            height: 300,
+            width: 400,
+            // backgroundColor: '#00F',
+          }}
+          onPress={() => null}
+        >
+          <Image
+            source={RecycleWheel}
+            style={{ height: '100%', width: '100%', resizeMode: 'contain' }}
+          />
+        </TouchableOpacity>
+      )}
       <TouchableOpacity
         style={{
           position: 'absolute',
           top: 570,
           left: 150,
           elevation: 200,
-          height: 110,
-          width: 325,
+          height: 60,
+          width: 65,
+          // backgroundColor: '#F00',
         }}
         onPress={() => recycleHandler()}
       >
