fe55f9c23dc74febb1c7b4dd098c9eb3a41b6c4a
Ren Xiang <foorenxiang@gmail.com>
2021-08-28 10:55:02 +0800

    reshuffled screens for tab navigator

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/Home';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/Boilerplate';
+import LearnLanding from './screens/LearnLanding';
+import LogNewItem from './screens/LogNewItem';
+import Profile from './screens/Profile';
+import ElectronicStatus from './screens/ElectronicStatus';
+import LearnArticleViewer from './screens/LearnArticleViewer';
+import ObjectDetection from './screens/ObjectDetection';
+import ViewLog from './screens/ViewLog';
 
-const { Navigator, Screen: StackScreen } = createNativeStackNavigator();
+const { Navigator, Screen: TabScreen } = createBottomTabNavigator();
 
 const Routes = () => (
   <NavigationContainer>
     <Navigator initialRouteName="Home">
-      <StackScreen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
+      <TabScreen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
+      <TabScreen name="Learn" component={LearnLanding} options={{}} />
+      <TabScreen name="Log New Electronic" component={LogNewItem} options={{}} />
+      <TabScreen name="Profile" component={Profile} options={{}} />
     </Navigator>
   </NavigationContainer>
 );
