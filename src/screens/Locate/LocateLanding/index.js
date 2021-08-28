5ee262f683564ff1001cd6ea92cf35f6d6d9e4f8
Ren Xiang <foorenxiang@gmail.com>
2021-08-29 04:49:44 +0800

    locate screen first draft

--- src/screens/Locate/LocateLanding/index.js
+++ src/screens/Locate/LocateLanding/index.js
@@ -1,5 +1,59 @@
-import React from 'react';
+/* eslint-disable global-require */
+import React, { useState } from 'react';
+import { View, Image, TouchableOpacity } from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+import LocateBG from './LocateBG.jpg';
+import Image1 from './Image1.png';
+import Image2 from './Image2.png';
 
-const LocateLanding = () => <></>;
+const carouselItems = [Image1, Image2];
+
+const LocateLanding = ({ navigation: { navigate } }) => {
+  const carouselRenderItem = ({ item }) => (
+    <View
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F00',
+        height: 130,
+      }}
+    >
+      <Image
+        source={item}
+        style={{ resizeMode: 'contain', height: '100%', width: '50%', backgroundColor: '#00F' }}
+      />
+    </View>
+  );
+
+  return (
+    <>
+      <Image
+        source={LocateBG}
+        style={{ flex: 1, width: '100%', height: '100%', resizeMode: 'contain' }}
+      />
+      <View
+        style={{
+          position: 'absolute',
+          top: 80,
+          left: 38,
+          justifyContent: 'center',
+          elevation: 100,
+          alignItems: 'center',
+          backgroundColor: '#000',
+          height: 130,
+          width: '80%',
+        }}
+      >
+        <Carousel
+          layout="default"
+          data={carouselItems}
+          sliderWidth={350}
+          itemWidth={300}
+          renderItem={carouselRenderItem}
+        />
+      </View>
+    </>
+  );
+};
 
 export default LocateLanding;
