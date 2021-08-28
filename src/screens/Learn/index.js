201dc8232e7722a29e5eb1a0908198917ecd88ba
Ren Xiang <foorenxiang@gmail.com>
2021-08-29 04:17:07 +0800

    added tab navigator icons

--- src/screens/Learn/index.js
+++ src/screens/Learn/index.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react';
+import { Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LearnLanding from './LearnLanding';
@@ -7,6 +8,10 @@ import AppContext from '../../utils/AppContext';
 import Log from '../Log';
 import Profile from '../Profile';
 import Locate from '../Locate';
+import LearnImage from './Learn.png';
+import LocationImage from './Location.png';
+import LogImage from './Log.png';
+import ProfileImage from './Profile.png';
 
 const { Navigator: StackNavigator, Screen: StackScreen } = createNativeStackNavigator();
 const { Navigator: TabNavigator, Screen: TabScreen } = createBottomTabNavigator();
@@ -28,6 +33,7 @@ const LearnTabs = ({ navigation }) => {
   useEffect(() => {
     setTopNavigation(navigation);
   }, []);
+
   return (
     <TabNavigator
       initialRouteName="Learn"
@@ -35,10 +41,26 @@ const LearnTabs = ({ navigation }) => {
         headerShown: false,
       }}
     >
-      <TabScreen name="Learn" component={Learn} options={{}} />
-      <TabScreen name="Locate" component={Locate} />
-      <TabScreen name="Log" component={Log} options={{}} />
-      <TabScreen name="Profile" component={Profile} options={{}} />
+      <TabScreen
+        name="Learn"
+        component={Learn}
+        options={{ tabBarIcon: () => <Image source={LearnImage} /> }}
+      />
+      <TabScreen
+        name="Locate"
+        component={Locate}
+        options={{ tabBarIcon: () => <Image source={LocationImage} /> }}
+      />
+      <TabScreen
+        name="Log"
+        component={Log}
+        options={{ tabBarIcon: () => <Image source={LogImage} /> }}
+      />
+      <TabScreen
+        name="Profile"
+        component={Profile}
+        options={{ tabBarIcon: () => <Image source={ProfileImage} /> }}
+      />
     </TabNavigator>
   );
 };
