a16df6c058bebc22bf9eae7c7c82c9d53b4042cf
Ren Xiang <foorenxiang@gmail.com>
2021-08-29 03:36:40 +0800

    updated homescreen

--- src/screens/Home/HomeLanding/index.js
+++ src/screens/Home/HomeLanding/index.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Image, ScrollView, TouchableOpacity } from 'react-native';
+import AppContext from '../../../utils/AppContext';
+import Home from './Home.png';
 
-const HomeLanding = () => <></>;
+const HomeLanding = () => {
+  const {
+    topNavigation: { navigate },
+  } = useContext(AppContext);
+  return (
+    <ScrollView
+      style={{ flex: 1, width: '100%' }}
+      contentContainerStyle={{ flex: 1, width: '100%' }}
+    >
+      <Image source={Home} style={{ flex: 1, width: '100%', height: 400, resizeMode: 'center' }} />
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          top: 368,
+          left: 25,
+          elevation: 100,
+          height: 110,
+          width: 325,
+          backgroundColor: '#DDD',
+          opacity: 0,
+        }}
+        onPress={() => navigate('Learn')}
+      />
+    </ScrollView>
+  );
+};
 
 export default HomeLanding;
