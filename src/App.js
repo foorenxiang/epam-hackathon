592f98297cda9245275a37e7c76f2363fe8d7123
Ren Xiang <foorenxiang@gmail.com>
2021-08-28 14:33:55 +0800

    added safeareaview

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { registerRootComponent } from 'expo';
+import { SafeAreaView } from 'react-native';
 import { Provider as ReduxProvider } from 'react-redux';
 import store from './store/store';
 import Routes from './Routes';
 import { AppContextProvider as ReactContextProvider } from './utils/AppContext';
+import { safeAreaViewStyles } from './styles/appStyles';
 
 const App = () => (
   <ReduxProvider store={store}>
     <ReactContextProvider>
-      <Routes />
+      <SafeAreaView style={safeAreaViewStyles}>
+        <Routes />
+      </SafeAreaView>
     </ReactContextProvider>
   </ReduxProvider>
 );
