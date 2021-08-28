b19a78d5c9e910b8b4bc1ca2083a0bbcd91441dd
Ren Xiang <foorenxiang@gmail.com>
2021-08-28 14:44:31 +0800

    setup navigation from log landing to object detection

--- src/screens/Log/LogNewItem/index.js
+++ src/screens/Log/LogNewItem/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { View, Button, StyleSheet } from 'react-native';
 
-const LogNewItem = () => <></>;
+const LogNewItem = ({ navigation: { navigate } }) => (
+  <View>
+    <Button onPress={() => navigate('ObjectDetection')} title="Upload Photo" color="#00F" />
+  </View>
+);
+
+const styles = StyleSheet.create({});
 
 export default LogNewItem;
--- src/screens/Log/ViewLogLanding/index.js
+++ src/screens/Log/ViewLogLanding/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { View, Button, StyleSheet } from 'react-native';
 
-const ViewLogLanding = () => <></>;
+const ViewLogLanding = ({ navigation: { navigate } }) => (
+  <View>
+    <Button onPress={() => navigate('LogNewItem')} title="Log New Item" color="#00F" />
+  </View>
+);
+
+const styles = StyleSheet.create({});
 
 export default ViewLogLanding;
