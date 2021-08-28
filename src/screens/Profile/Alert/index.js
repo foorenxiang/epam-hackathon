ec4ed34ad70a92e757bc1c213b7ad9f33338e94b
Ren Xiang <foorenxiang@gmail.com>
2021-08-29 05:17:09 +0800

    fixed profile alert page for ios

--- src/screens/Profile/Alert/index.js
+++ src/screens/Profile/Alert/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Image, ScrollView, TouchableOpacity, ImageBackground } from 'react-native';
-import Img from './alert.png';
+import Img from './alert.jpg';
 
 const Alert = ({ navigation: { navigate } }) => {
   return (
     <ScrollView
       style={{ flex: 1, width: '100%' }}
-      contentContainerStyle={{ flex: 1, width: '100%' }}
+      contentContainerStyle={{ flex: 1, width: '100%', paddingTop: 0 }}
     >
       <Image source={Img} style={{ flex: 1, width: '100%', height: 400, resizeMode: 'center' }} />
       <TouchableOpacity
         style={{
           position: 'absolute',
-          top: 368,
+          top: 500,
           left: 25,
           elevation: 100,
           height: 110,
           width: 325,
-          backgroundColor: '#f00',
+          // backgroundColor: '#f00',
           opacity: 0.8,
         }}
         onPress={() => navigate('ProfileLanding')}
